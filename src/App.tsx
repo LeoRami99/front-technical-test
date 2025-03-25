9a8dded8bad59df89b0ea83149d8dbcd30eef977
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-25 07:06:46 -0500

    Add CreditCardValidator and Modal components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import CreditCardValidator from "./components/CreditCardValidator/CreditCardValidator";
+import Modal from "./components/Modal/Modal";
 import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
 
@@ -8,6 +10,8 @@ function App() {
 			<Navbar />
 			<div>
 				<Products />
+
+				<Modal id='validation-credit-card' children={<CreditCardValidator />} />
 			</div>
 		</>
 	);
