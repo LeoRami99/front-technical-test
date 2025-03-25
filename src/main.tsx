73a5f4dbdb192b3d4eeda79ec526349c3381dda8
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-25 16:42:29 -0500

    Add routing for transaction page in main application

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,17 @@ import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import "./index.css";
 import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import TransactionPage from "./pages/transaction.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Toaster />
-		<App />
+		<BrowserRouter>
+			<Toaster />
+			<Routes>
+				<Route path='/' element={<App />} />
+				<Route path='/transaction/:idTransaction' element={<TransactionPage />} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>
 );
