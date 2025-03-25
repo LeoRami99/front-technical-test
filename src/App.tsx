a5d1c6bfed861b233f74c4cdd10cabe0fb464e48
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-25 14:38:39 -0500

    Refactor Modal component to handle multiple steps in the transaction process

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import "./App.css";
 import CreditCardValidator from "./components/CreditCardValidator/CreditCardValidator";
+import FormTermConditions from "./components/FormTermConditions/FormTermConditions";
 import Modal from "./components/Modal/Modal";
 import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
+import { useSteps } from "./stores/useTransaction";
 
 function App() {
+	const { step } = useSteps();
+
 	return (
 		<>
 			<Navbar />
 			<div>
 				<Products />
 
-				<Modal id='validation-credit-card' children={<CreditCardValidator />} />
+				<Modal id='start-transaction'>
+					{step === 1 && <h1>Step 1</h1>}
+					{step === 2 && <FormTermConditions />}
+					{step === 3 && <CreditCardValidator />}
+				</Modal>
 			</div>
 		</>
 	);
