1ed643e1c5ccf7b94e248d0ef291bae18dbdddd1
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-24 22:39:04 -0500

    Add initial CSS and configuration files for React and Tailwind CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Products from "./components/Products/Products";
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<div>
+				<Products />
+			</div>
+		</>
+	);
+}
+
+export default App;
