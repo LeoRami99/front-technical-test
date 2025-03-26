653eec4d96ae86b81eae45f6073bd76d9b91466d
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-25 22:02:09 -0500

    Add InternalTransaction interface for enhanced transaction management

--- src/interfaces/transaction.interface.ts
+++ src/interfaces/transaction.interface.ts
@@ -10,3 +10,17 @@ export interface SendTransaction {
 	acceptance_token: string;
 	installments: number;
 }
+
+export interface InternalTransaction {
+	id: string;
+	referenceInternalTransaction: string;
+	amount: number;
+	status: string;
+	idExternalTransaction: string;
+	userId: string;
+	methodPayment: string;
+	productId: string;
+	price: number;
+	createdAt: string;
+	updatedAt: string;
+}
