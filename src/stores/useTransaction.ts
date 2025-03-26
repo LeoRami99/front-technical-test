e6a240ab530a0a3778e8fa8e68ac4ab59cb25bfd
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-25 22:02:22 -0500

    Refactor useTransaction store to initialize tokenCard and add name and installments properties in ProductState

--- src/stores/useTransaction.ts
+++ src/stores/useTransaction.ts
@@ -1,12 +1,12 @@
 import { create } from "zustand";
 
 interface CardTokenState {
-	tokenCard: string | null;
+	tokenCard: string;
 	setTokenCard: (token: string) => void;
 }
 
 export const useCardToken = create<CardTokenState>((set) => ({
-	tokenCard: null,
+	tokenCard: "",
 	setTokenCard: (token: string) => set({ tokenCard: token }),
 }));
 
@@ -31,27 +31,35 @@ export const useSteps = create<StepsState>((set) => ({
 }));
 
 interface ProductState {
+	name: string;
 	amount: number;
 	userId: string;
 	methodPayment: string;
 	productId: string;
 	price: number;
+	installments: number;
+	setName: (name: string) => void;
 	setAmount: (amount: number) => void;
 	setUserId: (userId: string) => void;
 	setMethodPayment: (method: string) => void;
 	setProductId: (productId: string) => void;
 	setPrice: (price: number) => void;
+	setInstallments: (installments: number) => void;
 }
 
 export const useProduct = create<ProductState>((set) => ({
+	name: "",
 	amount: 1,
 	userId: "f788b0a5-650e-458c-bef9-f6ce9bd75e1b",
 	methodPayment: "CARD",
 	productId: "",
 	price: 0,
+	installments: 1,
+	setName: (name: string) => set({ name }),
 	setAmount: (amount: number) => set({ amount }),
 	setUserId: (userId: string) => set({ userId }),
 	setMethodPayment: (method: string) => set({ methodPayment: method }),
 	setProductId: (productId: string) => set({ productId }),
 	setPrice: (price: number) => set({ price }),
+	setInstallments: (installments: number) => set({ installments }),
 }));
